b2b57a0e7fa8e6822d910ecbc4e3b1bce841899e
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-08-17 12:28:54 -0300

    adicionando exeption

--- src/main/kotlin/br/upf/sistemaeventos/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/br/upf/sistemaeventos/exceptions/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package br.upf.sistemaeventos.exceptions
 import br.upf.sistemaeventos.dtos.ErrorResponseDTO
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -34,6 +35,24 @@ class ExceptionHandler {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             massage = exception.message ?: "Erro desconhecido!",
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorResponseDTO {
+        val errorMessage = hashMapOf<String, String>()
+        exception.bindingResult.fieldErrors.forEach {
+            errorMessage[it.field] = it.defaultMessage ?: "Erro não identificado"
+        }
+        return ErrorResponseDTO(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            massage = errorMessage.toString(),
             path = request.servletPath
         )
     }
