f68d80da850c5e7f7d8f7cf8a933daa6e4df2390
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-10-20 15:35:22 -0300

    fazendo as alterações necessárias para comunicar com o nosso crud no frontend

--- src/main/kotlin/br/upf/sistemaeventos/config/CorsConfig.kt
+++ src/main/kotlin/br/upf/sistemaeventos/config/CorsConfig.kt
@@ -13,7 +13,7 @@ class CorsConfig {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000") // substitua pelo endereço do seu frontend
+                    .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                     .allowCredentials(true)
             }
